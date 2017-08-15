dee7345d30c8d88e3a5a00a1800c475f6aecce57
antonis179 <antonis179@gmail.com>
2017-08-15 20:14:52 +0300

    Structure changes
    - Added better .gitignore file that covers Android, iOS, inteliJ ignored files
    - Added iOS project
    - Removed release folder (releases will be uploaded seperately from now on)

---
+++ linker-ios-client/Linker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Linker
+//
+//  Created by Antonis Moustakos on 15/08/2017.
+//  Copyright © 2017 Antonis Moustakos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ linker-ios-client/Linker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Linker
+//
+//  Created by Antonis Moustakos on 15/08/2017.
+//  Copyright © 2017 Antonis Moustakos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
